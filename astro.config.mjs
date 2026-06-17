43e17f03f72b696ce64d97332fa6126c63769f8f
articwnd <rchrdxiong@gmail.com>
2026-06-17 01:24:28 -0400

    feat:added projects tab

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-// astro.config.mjs
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://articwnd.github.io',
-  // No base needed since repo IS username.github.io
-});
